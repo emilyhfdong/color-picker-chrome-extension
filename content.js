f0a98f2453cdd8f07dc44b80c21b18306b81f68b
Emily Dong <emilyhfdong@gmail.com>
2022-03-02 14:57:01 -0500

    display saved color palettes

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 const CANVAS_ID = "html-canvas"
 const SWATCH_ID = "color-swatch"
+const SWATCH_WIDTH = 50
 const SIDEPANEL_ID = "color-palette-sidepanel"
 
 const styles = `
@@ -7,12 +8,20 @@ const styles = `
     position: fixed;
     top: 0px;
     left: 0px;
-    height: 50px;
-    width: 50px;
-    border-radius: 25px;
     z-index: 10000000;
-    box-shadow: 0px 0px 8px 3px rgba(0, 0, 0, 0.05);
+  }
+  .color-circle {
+    box-shadow: 0px 0px 8px 3px rgba(0, 0, 0, 0.1);
     background-color: white;
+    height: ${SWATCH_WIDTH}px;
+    width: ${SWATCH_WIDTH}px;
+    border-radius: 25px;
+    margin-right: 10px;
+  }
+  #${CANVAS_ID} {
+    position: fixed;
+    top: 0px;
+    opacity: 0;
   }
   #${SIDEPANEL_ID} {
     position: fixed;
@@ -22,51 +31,128 @@ const styles = `
     background-color: white;
     z-index: 1000000;
     width: 25vw;
-    box-shadow: 5px 0px 8px 3px rgba(0, 0, 0, 0.1);
+    box-shadow: 5px 0px 8px 3px rgba(0, 0, 0, 0.06);
+    font-family: 'Roboto', sans-serif;
+  }
+  #${SIDEPANEL_ID} h3 {
+    font-weight: 400;
+    letter-spacing: 0.05em;
+    font-family: 'Oswald', sans-serif;
+    padding: 0 1rem 0 1rem;
+  }
+  .palette {
+    padding: 0 1rem 1rem 1rem;
+    background-color: white;
+    border-top: 1px solid #F8F8F8;
+    cursor: pointer;
+  }
+  .palette:hover {
+    background-color: #F8F8F8;
+  }
+  .active-palette {
+    background-color: #F8F8F8;
+  }
+  .colors-container {
+    display: flex;
+    flex-wrap: wrap;
   }
 `
 
+const createPaletteElement = ({ name, colors }, idx) => {
+  const paletteTitle = document.createElement("p")
+  paletteTitle.classList.add("palette-title")
+  paletteTitle.innerText = name
+
+  const paletteDiv = document.createElement("div")
+  paletteDiv.classList.add("palette")
+  if (idx === 0) {
+    paletteDiv.classList.add("active-palette")
+  }
+  paletteDiv.appendChild(paletteTitle)
+
+  const colorsContainer = document.createElement("div")
+  colorsContainer.classList.add("colors-container")
+  paletteDiv.appendChild(colorsContainer)
+
+  colors.forEach((color) => {
+    const colorDiv = document.createElement("div")
+    colorDiv.classList.add("color-circle")
+
+    colorDiv.style.backgroundColor = color
+    colorsContainer.appendChild(colorDiv)
+  })
+
+  return paletteDiv
+}
+
+const MOCK_PALETTES = [
+  { name: "palette 1", colors: ["#F5891B", "#1C3984", "#23A0DD", "#EE3D55"] },
+  { name: "palette 2", colors: ["#EAB39D", "#E62E3B", "#C5CAB3", "#135142"] },
+]
+
+const cleanUp = () => {
+  document.getElementById(SWATCH_ID).remove()
+  document.getElementById(CANVAS_ID)?.remove()
+  document.getElementById(SIDEPANEL_ID)?.remove()
+}
+
 chrome.runtime.onMessage.addListener(() => {
-  // clean up old stuff
   if (document.getElementById(SWATCH_ID)) {
-    document.getElementById(SWATCH_ID).remove()
-    if (document.getElementById(CANVAS_ID)) {
-      document.getElementById(CANVAS_ID).remove()
-    }
+    // TODO - remove event handlers
+    cleanUp()
     return
   }
   // inject new styles
   const styleElement = document.createElement("style")
   styleElement.textContent = styles
+  const fontsLink = document.createElement("link")
+  fontsLink.href =
+    "https://fonts.googleapis.com/css2?family=Oswald&family=Roboto&display=swap"
+  fontsLink.rel = "stylesheet"
   document.head.append(styleElement)
+  document.head.append(fontsLink)
 
   // create sidepanel
-
   document.body.style.width = "75vw"
+  const sidepanelTitle = document.createElement("h3")
+  sidepanelTitle.innerText = "COLOR PALETTES"
   const sidepanel = document.createElement("div")
-  sidepanel.id = SIDEPANEL_ID
+  sidepanel.setAttribute("id", SIDEPANEL_ID)
+  sidepanel.appendChild(sidepanelTitle)
   document.body.appendChild(sidepanel)
+
+  // fill sidepanel with saved palettes
+  chrome.storage.sync.get(["palettes"], function (result) {
+    if (result.palettes?.length) {
+      result.palettes.forEach((palette, idx) => {
+        createPaletteElement(palette, idx)
+        sidepanel.appendChild(paletteDiv)
+      })
+    }
+  })
 
   // create swatch element
   const swatchDiv = document.createElement("div")
-  swatchDiv.id = SWATCH_ID
+  swatchDiv.setAttribute("id", SWATCH_ID)
+  swatchDiv.classList.add("color-circle")
   document.body.appendChild(swatchDiv)
 
   html2canvas(document.body).then((canvas) => {
     canvas.id = CANVAS_ID
-    canvas.style.position = "fixed"
-    canvas.style.top = "0"
-    canvas.style.opacity = "0"
     document.body.appendChild(canvas)
 
     document.addEventListener("mousemove", (ev) => {
+      if (ev.pageX > document.body.clientWidth) {
+        return
+      }
       const context = document.getElementById(CANVAS_ID).getContext("2d")
       const data = context.getImageData(ev.pageX * 2, ev.pageY * 2, 1, 1).data
       const color = `rgb(${data[0]}, ${data[1]}, ${data[2]})`
-      swatchDiv.style.top = ev.pageY + "px"
-      swatchDiv.style.left = ev.pageX + "px"
+      if (ev.pageX + SWATCH_WIDTH < document.body.clientWidth) {
+        swatchDiv.style.left = ev.pageX + "px"
+        swatchDiv.style.top = ev.pageY + "px"
+      }
       swatchDiv.style.backgroundColor = color
     })
-    document.addEventListener("click", () => {})
   })
 })
